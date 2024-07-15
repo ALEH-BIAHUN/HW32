d38e515e640325245613b2567dc0698e06d9f19a
Aleh Biahun <stones2875@gmail.com>
2024-07-15 12:59:23 +0200

    Solution task number 32.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,23 @@
 package org.example;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "https://tvn24.pl/";
         try {
+            String url = "https://tvn24.pl/";
             Document document = Jsoup.connect(url).get();
-            Elements elements = document.select("div .article-header");
-            for (Element element : elements) {
-                System.out.println(element.text());
-            }
+            Elements elements = document.select("div .teaser-wrapper");
+
+            elements.forEach(element -> {
+                        String title = element.text();
+                        String link = element.select("a").attr("href");
+                System.out.println("Title: " + title);
+                System.out.println("Link: " + link);
+                    });
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
